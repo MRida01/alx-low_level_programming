5326354c8e2464db2657839c777b4b2cefb15ffc
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-10-06 17:11:29 -0700

    325

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/*
+ *comment
+*/
 int main(void)
 {
-  char string[] = "\"Programming is like building a multilingual puzzle";
-  puts(string);
-  return (0);
+char string[] = "\"Programming is like building a multilingual puzzle";
+
+puts(string);
+return (0);
 }
