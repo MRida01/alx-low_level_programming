2afefcb16e3701211376001b9fef98f27f56a065
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-10-09 15:32:59 -0700

    updates

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - writes out the alphabet fron a to z
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char n;
+
+for (n = 'a'; n <= 'z'; n++)
+{
+printf("%c", n);
+}
+printf("\n");
+return (0);
+}
