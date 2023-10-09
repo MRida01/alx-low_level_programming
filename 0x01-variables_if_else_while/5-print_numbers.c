5a7bc0c2286089dedde2ed17fbc3bcc00918cccc
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-10-09 16:03:27 -0700

    updates

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - lists numbers from 0 to 10
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+
+for (n = 0; n <= 10; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
