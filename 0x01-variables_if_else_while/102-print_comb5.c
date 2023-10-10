9e4006ce326a5b669fc392ca6be5b0f817d9b9ae
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-10-09 18:17:25 -0700

    updates

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main - lists digit combinations
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+
+int m;
+
+int o;
+
+int p;
+
+for (n = 48; n <= 57; n++)
+{
+for (m = 48; m <= 57; m++)
+{
+for (o = n; o <= 57; n++)
+{
+for (p = m+1; p <= 57; m++)
+{
+putchar(n);
+putchar(m);
+putchar(32);
+putchar(o);
+putchar(p);
+if (n != 56)
+{
+	putchar(44);
+	putchar(32);
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
