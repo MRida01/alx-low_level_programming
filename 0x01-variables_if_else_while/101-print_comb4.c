a48bf304b9aac804071f0faab1340278a1f08325
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-10-09 17:58:57 -0700

    updates

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,16 +18,16 @@ for (m = n; m <= 56; m++)
 {
 for (o = m; o <= 57; o++)
 {
-if (m != n || m != o || n != o)
+if (m != n && m != o && n != o)
 {
-        putchar(n);
-        putchar(m);
+	putchar(n);
+	putchar(m);
 	putchar(o);
-        if (n != 55)
-        {
-                putchar(44);
-                putchar(32);
-        }
+	if (n != 55)
+	{
+		putchar(44);
+		putchar(32);
+	}
 }
 }
 }
