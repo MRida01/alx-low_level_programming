45633698c1cc08445c972722b99c0852e5bb59dc
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-10-09 17:18:52 -0700

    updates

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-const char *n = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 write(1, n, strlen(n));
 return (1);
