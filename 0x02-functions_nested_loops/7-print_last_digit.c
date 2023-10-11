52efb1ec7493c7bb20e93e38a1916195865fb4f5
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-10-11 15:42:15 -0700

    updates

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit of int
+ * @_: int to be checked
+ *
+ * Return: last digit of int
+ */
+int print_last_digit(int _)
+{
+int m;
+
+m = _ % 10;
+m = m + 48;
+_putchar(m);
+m = m - 48;
+return (m);
+}
