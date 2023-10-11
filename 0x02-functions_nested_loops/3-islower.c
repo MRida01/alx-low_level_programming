899cdacd2e1df492adf9cb7587a613bf5a1ce960
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-10-10 17:34:40 -0700

    updates

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * _islower - checks if a letter is lowercase
+ * @c: character to be checked
  *
  * Return: 0 or 1
  */
 int _islower(int c)
 {
-if ( c <= 122 && c >= 97)
+if (c <= 122 && c >= 97)
 {
 	return (1);
 }
