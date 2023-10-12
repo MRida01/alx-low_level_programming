c212060d2052e78e17b678f9df9d3468fb00d160
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-10-12 16:07:49 -0700

    updates

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - checks if a letter is uppercase
+ * @c: character to be checked
+ *
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+if (c <= 90 && c >= 65)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}	
