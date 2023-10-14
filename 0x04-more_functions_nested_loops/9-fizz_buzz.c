2f0b40cf8d8773c008fb7a781abddfeda8f6347e
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-10-13 19:45:05 -0700

    updates

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - FizzBuzz challenge
+ */
+void main(void)
+{
+int n;
+
+for (n = 0; n <= 100; n++)
+{
+if (n % 15 == 0)
+{
+printf("FizzBuzz");
+}
+else if (n % 5 == 0)
+{
+printf("Buzz");
+}
+else if (n % 3 == 0)
+{
+printf("Fizz");
+}
+else
+{
+printf("%d", n);
+}
+if (n != 100)
+{
+printf("\n");
+}
+}
