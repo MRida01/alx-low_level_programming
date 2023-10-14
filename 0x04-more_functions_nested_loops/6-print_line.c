2888cde2d35e79e94962c386f53d3e6095086284
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-10-13 18:57:31 -0700

    updates

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - prints line of desired length
+ * @n: line length
+ */
+void print_line(int n)
+{
+int m;
+
+for (m = 0; m < n; n++)
+{
+_putchar(95);
+}
+_putchar(10);
+}
