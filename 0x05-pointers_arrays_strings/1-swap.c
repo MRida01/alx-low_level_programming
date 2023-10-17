300ae75b7d7b5a935298d95ba3b6a917aec59693
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-10-17 15:24:54 -0700

    updates

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swaps two characters 
+ * @a: int to be swapped
+ * @b: int to be swapped
+ */
+void swap_int(int *a, int *b)
+{
+int c;
+
+c = *a;
+*a = *b;
+*b = c;
+}
