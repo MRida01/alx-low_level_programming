cfe6b8bfda341c6e1959ab82de892e8512dee8b1
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-10-20 10:45:21 -0700

    updates

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcat - attaches a string to the end of another
+ * @dest: destination string
+ * @src: string to be attached
+ *
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+if (dest == NULL || src == NULL) {
+return;
+}
+while (*dest != '\0') {
+dest++;
+}
+while (*src != '\0') {
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return dest;
+}
