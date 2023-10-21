a2728595cf3016b62b30626a7de9d6a861309ae2
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-10-20 20:09:51 -0700

    updates

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#imclude "main.h"
+/**
+ * string_toupper - turns lowercase letters into uppercase
+ * @s: string to capitalise
+ *
+ * Return: modified string
+ */
+char *string_toupper(char *s)
+{
+char *n = s;
+
+while (*s != '\0')
+{
+if (*s >= 97 && *s <= 122)
+{
+  *s = *s - 97;
+  *s = *s + 65;
+}
+s++;
+}
+return (n);
+}
