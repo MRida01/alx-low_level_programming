5c385a76059232656123157db37a19738ad0035d
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-10-24 16:25:48 -0700

    updates

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,37 @@
+#include <stddef.h>
+#include "main.h"
+/**
+ * _strspn - searches a string for characters and counts them
+ * @s: address to search
+ * @accept: characters to look for
+ *
+ * Return: number of characters
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int m = 0;
+
+while (*s != '\0')
+{
+int n = 0;
+
+char *c = accept;
+
+while (*c != '\0')
+{
+if (*s == *c)
+{
+n = 1;
+break;
+}
+c++;
+}
+if (!n)
+{
+break;
+}
+m++;
+s++;
+}
+return (m);
+}
