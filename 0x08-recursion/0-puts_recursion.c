b6d6640e673dcbb5b5984eb027bb9bfd02aaec10
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-10-25 13:02:45 -0700

    updates

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - has the same function as puts
+ * @s: string to be printed
+ */
+void _puts_recursion(char *s)
+{
+while (*s != '\0')
+{
+write(1, str, 1);
+s++;
+}
+write(1, "\n", 1);
+}
