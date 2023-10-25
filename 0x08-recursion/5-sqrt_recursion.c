7110e61f6d3c3c1ca0d77568c8a4b89f92b5cd7f
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-10-25 16:54:54 -0700

    updates

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * realsqrt - finds the square root through recursion
+ * @n: number to use
+ * @m: minimum possible value
+ * @l: maximum possible value
+ *
+ * Retrun: square root or -1
+ */
+int realsqrt(int n, int m, int l)
+{
+int i = m + (l - m) / 2;
+
+long long j = (long long)i * i;
+    
+if (j == n)
+{
+return i;
+}
+else if (j < n && (i + 1) * (i + 1) > n)
+{
+return (-1)
+}
+else if (j > n)
+{
+return realsqrt(n, m, i - 1);
+}
+else
+{
+return realsqrt(x, i + 1, l);
+}
+}
+/**
+ * _sqrt_recursion - finds square root by calling a recursive function
+ * @n: number to use
+ *
+ * Return: square root or -1
+ */
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+else if (n <= 1)
+{
+return (n);
+}
+return realsqrt(n, 0, n);
+}
