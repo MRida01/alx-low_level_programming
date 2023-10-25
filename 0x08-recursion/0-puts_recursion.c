37e64f4f64e675a4966a8bf2e7e121ed25afbed6
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-10-25 13:05:22 -0700

    updates

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 /**
  * _puts_recursion - has the same function as puts
  * @s: string to be printed
@@ -7,7 +8,7 @@ void _puts_recursion(char *s)
 {
 while (*s != '\0')
 {
-write(1, str, 1);
+write(1, s, 1);
 s++;
 }
 write(1, "\n", 1);
