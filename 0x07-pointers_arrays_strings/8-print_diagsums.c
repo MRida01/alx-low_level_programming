58d79a88c47507aeb613d12f10dc0acd6e1fd5c0
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-10-24 18:14:02 -0700

    updates

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,22 +9,17 @@ void print_diagsums(int *a, int size)
 {
 int i;
 
-int j;
-
 int n = 0;
 
 int m = 0;
 
 for (i = 0; i < size; i++)
 {
-n = n + a[i][i];
+n = n + a[i * size + i];
 }
 for (i = 0; i < size; i++)
 {
-  for (j = size - 1; j >= 0; j--)
-{
-m = m + a[i][j];
-}
+m = m + a[i * size + (size - 1 - i)];
 }
 printf("%d, %d\n", n, m);
 }
