985d8f06d88e81da8e223d8c81a40d5fcbafe4f6
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-10-31 14:16:42 -0700

    updates

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "main.h"
+/**
+ * _strlen - checks string length
+ * @s: string to be checked
+ *
+ * Return: length of s
+ */
+int _strlen(char *s)
+{
+int n;
+
+n = 0;
+while (s[n] != '\0')
+{
+n++;
+}
+return (n);
+}
+/**
+ * _strdup - copies string and returns pointer to array
+ * @str: string to use
+ *
+ * Return: pointer to array or NULL if error
+ */
+char *_strdup(char *str);
+{
+int i;
+
+char *array;
+
+int size = _strlen(str);
+
+if (size <= 0)
+{
+return (NULL);
+}
+array = malloc(sizeof(char) * (size + 1));
+if (array == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < size; i++)
+{
+array[i] = *str;
+}
+return (array);
+}
