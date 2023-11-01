95ebc04f40e6b65f5eb28c6047723c37053a5663
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-11-01 16:34:19 -0700

    updates

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,22 @@
 #include <unistd.h>
+#include <string.h>
 /**
  * main - prints out the program name
  *
  * Return: 0
  */
-int main()
+int main(int argc, char *argv[])
 {
+char n = '\n';
+
 char *str = argv[0];
     
 size_t len = strlen(str);
 
+if (argc > 0)
+{
 write(1, str, len);
-return 0;
+write(1, &n, 1);
+}
+return (0);
 }
