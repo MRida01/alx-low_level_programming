d37de7ddf8979857096c52e3552657f6288308e0
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-11-03 18:22:05 -0700

    updates

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,11 @@
 #include <stdlib.h>
-
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: number of elements
+ * @size: size of elements
+ *
+ * Return: pointer or NULL
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 void *p;
