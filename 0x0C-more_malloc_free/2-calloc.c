d0bbcfd88a774ee24864778941a3144dfbe15c64
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-11-03 18:13:36 -0700

    updates

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *ptr;
+
+unsigned char *b;
+
+unsigned int i;
+
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
+p = malloc(nmemb * size);
+if (ptr == NULL)
+{
+return (NULL);
+}
+b = (unsigned char *)p;
+for (i = 0; i < nmemb * size; i++)
+{
+b[i] = 0;
+}
+return (n);
+}
