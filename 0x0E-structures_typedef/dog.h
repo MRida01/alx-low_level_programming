77a0792a464fc4414edb7be5cd1e6047c815f385
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-11-07 15:42:19 -0800

    updates

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - idintifiers of a dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * contains traits that identify a dog
+ */
+struct dog
+{
+char name;
+float age;
+char owner;
+};
+
+#endif
