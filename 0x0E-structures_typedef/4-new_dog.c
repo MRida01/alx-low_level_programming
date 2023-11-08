2ea7e6f792a1c80500efb2e7f3b753e7f4ea44b8
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-11-07 16:39:36 -0800

    updates

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <string.h>
+/**
+ * new_dog - creates a new dog variable
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Return: returns the new dog
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *newDog = (dog_t *)malloc(sizeof(dog_t));
