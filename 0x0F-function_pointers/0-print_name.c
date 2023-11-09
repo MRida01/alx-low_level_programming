24a5e03dcb88806124c955a06fabe9cfc42dd5bc
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-11-08 16:30:22 -0800

    updates

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name using the provided function
+ * @name: name to print
+ * @f: function to use
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name == NULL || f == NULL)
+{
+return;
+}
+f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef POINTERFUNCTION_H 
+#define POINTERFUNCTION_H
+
+void print_name(char *name, void (*f)(char *));
+void print_name_as_is(char *name);
+void print_name_uppercase(char *name);
+
+#endif
