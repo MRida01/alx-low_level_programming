47faed176ea89f11c3c7421fa826faa19736cedb
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-11-08 16:36:42 -0800

    updates

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 /**
  * print_name - prints a name using the provided function
  * @name: name to print
@@ -6,5 +7,9 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+if (name == NULL || f == NULL)
+{
+return;
+}
 f(name);
 }
