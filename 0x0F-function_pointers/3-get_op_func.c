ce9cce182ac3de254c63a9430636997d64b39c52
Mohamed Rida Oukili <mridaoukili@gmail.com>
2023-11-09 17:04:01 -0800

    updates

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * get_op_func - directs to the right opperator function
+ * @s: the operator as a string
+ * Return: pointer to the corresponding function or NULL
+ */
+int (*get_op_func(char *s))(int, int)
+{
+int i = 0;
+
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}};
+
+while (ops[i].op)
+{
+if (*(ops[i].op) == *s && !(*(s + 1)))
+{
+return ops[i].f;
+}
+i++;
+}
+printf("Error\n");
+exit(99);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - calculates the sum of a and b
+ * @a: first operand
+ * @b: second operand
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * op_sub - calculates the difference of a and b.
+ * @a: first operand
+ * @b: second operand
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - calculates the product of a and b.
+ * @a: first operand
+ * @b: second operand
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - calculates the result of the division of a by b.
+ * @a: first operand
+ * @b: second operand
+ * Return: result of the division of a by b
+ */
+int op_div(int a, int b)
+{
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a / b);
+}
+/**
+ * op_mod - calculates the remainder of the division of a by b.
+ * @a: first operand
+ * @b: second operand
+ * Return: remainder of the division of a by b
+ */
+int op_mod(int a, int b)
+{
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a % b);
+}
