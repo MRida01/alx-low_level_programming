315073786bb10ce2450f7c96b7440d6693ed93a5
Mohamed Rida Oukili <lolridalol@gmail.com>
2023-11-17 15:22:16 -0800

    updates

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: pointer to the pointer to the head of the list.
+ * @str: string to use for new node.
+ *
+ * Return: address of the new element, or NULL if it failed.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+list_t *new_node, *temp;
+
+if (str == NULL)
+return (NULL);
+new_node = malloc(sizeof(list_t));
+if (new_node == NULL)
+return (NULL);
+new_node->str = strdup(str);
+if (new_node->str == NULL)
+{
+free(new_node);
+return (NULL);
+}
+new_node->len = _strlen(str);
+new_node->next = NULL;
+if (*head == NULL)
+{
+*head = new_node;
+}
+else
+{
+temp = *head;
+while (temp->next != NULL)
+temp = temp->next;
+temp->next = new_node;
+}
+return (new_node);
+}
+/**
+ * _strlen - calculates the length of a string
+ * @str: String to use
+ *
+ * Return: length of the string
+ */
+size_t _strlen(const char *str)
+{
+size_t length = 0;
+
+while (str[length] != '\0')
+length++;
+return (length);
+}
