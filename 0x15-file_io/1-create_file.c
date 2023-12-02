dcd82e19977052d8c369bb20495072dfd3630bd7
Mohamed Rida Oukili <lolridalol@gmail.com>
2023-12-01 16:16:57 -0800

    updates

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@
 int create_file(const char *filename, char *text_content)
 {
 int fd, written_bytes;
-    
+
 if (filename == NULL)
 return (-1);
 fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
