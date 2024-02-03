6b8ef5968a91bd7f12655002863a874eb0fdb402
Mohamed Rida Oukili <lolridalol@gmail.com>
2024-02-02 18:52:40 -0800

    updates

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * hash_djb2 - runs the djb2 algorithm
+ * @str: hash table to use
+ *
+ * Return: djb2 algorithm result
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+  unsigned long int hash;
+  int c;
+
+  hash = 5381;
+  while ((c = *str++))
+    {
+      hash = ((hash << 5) + hash) + c;
+    }
+  return (hash);
+}
